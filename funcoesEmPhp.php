7d43d986e7d801ed8aee56b894594f353c44c988
Mentins <thiagomartinsmiranda.27.09@gmail.com>
2022-07-25 17:16:27 -0300

    Adicionando a função para envio de múltiplos arquivos

--- funcoesEmPhp.php
+++ funcoesEmPhp.php
@@ -16,3 +16,26 @@
         $valor = htmlspecialchars($valor);
         return $valor;
     }
+
+    // FUNÇÃO PARA ENVIO DE ARQUIVOS MÚLTIPLOS
+    function reArrayFiles(&$file_post) {
+
+        $file_ary = array();
+        $file_count = count($file_post['name']);
+        $file_keys = array_keys($file_post);
+    
+        for ($i=0; $i<$file_count; $i++) {
+            foreach ($file_keys as $key) {
+                $file_ary[$i][$key] = $file_post[$key][$i];
+            }
+        }
+    
+        return $file_ary;
+
+        /* 
+        Não esqueça de passar essa função ao seu array de arquivos (exemplo: $arrayDeArquivos = reArrayFiles($_FILES['arquivo']);) e combe esta função com um laço de repetição foreach, para que fique mais fácil caminhar sobre todo o array sem muitas dificuldades, poupando linhas de códigos, facilitando manutenção, ajudando o programador que irá rever seu código e poupando vida útil do teclado.
+        
+        May the force be with you...
+        */
+    }
+?>
